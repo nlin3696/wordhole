fa64c935908437b86f659949a22b5a550d5d4479
Noah Lin <38114559+nlin3696@users.noreply.github.com>
2022-03-14 19:23:41 -0400

    prettier set with single quote

--- wordhole/src/App.js
+++ wordhole/src/App.js
@@ -1,24 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import FlexView from "react-flexview";
+import './App.css';
+import FlexView from 'react-flexview';
 
 export default function App() {
   return (
     <div className="App">
-      <FlexView className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 123123123123123
-        </a>
-      </FlexView>
+      <FlexView className="App-header">letsss gooo</FlexView>
+      <FlexView width="1">I love Kennedii</FlexView>
     </div>
   );
 }
