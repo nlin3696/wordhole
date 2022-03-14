0616f5c8f3f2395352fb1919c6bf13b002a8c2e3
Noah Lin <38114559+nlin3696@users.noreply.github.com>
2022-03-14 19:51:26 -0400

    message to test deploy

--- wordhole/src/App.js
+++ wordhole/src/App.js
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <div className="App">
       <FlexView className="App-header">letsss gooo</FlexView>
-      <FlexView width="1">I love Kennedii</FlexView>
+      <FlexView
+        width="100%"
+        hAlignContent="center"
+        style={{ fontSize: '100px', fontFamily: 'cursive' }}
+      >
+        I love Kennedii
+      </FlexView>
     </div>
   );
 }
